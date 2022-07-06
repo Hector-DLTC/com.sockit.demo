5685757449f2f180682459138b205df29c7d9645
hdlt <hectoradltc@gmail.com>
2022-07-06 12:30:38 -0500

    se agrego el pojos de usuarios

---
+++ src/main/java/com/demo/sockit/SockitApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sockit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockitApplication.class, args);
+	}
+
+}

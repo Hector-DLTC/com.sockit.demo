cbe39dc479c67e293f1c1467a841ac6d64d60eb9
Kazuo <kazuominakata@gmail.com>
2022-07-06 17:04:19 -0500

    Se Modificó productos.java

--- src/main/java/com/demo/sockit/productos.java
+++ src/main/java/com/demo/sockit/productos.java
@@ -1,68 +1,71 @@
 package com.demo.sockit;
 
-public class productos {
+public class productos extends provedores {
 	private String nombre;
 	private String descripcion;
 	private String URL_imagen;
 	private String categoria;
 	private double precio;
-	private int id;
+	private int idproductos;
 	private static int total=0;
-	public productos(String nombre, String descripcion, String uRL_imagen, String categoria, double precio, int id) {
-		super();
+	public productos(int idDirecciones, String pais, String estado, String municipio, String cpostal, String calle,
+			String numeroext, int idUsuarios, String usuario, String correo, String contraseña, int idProvedor, int tel,
+			String ine, String rfc, String dirPago, String nombre, String descripcion, String uRL_imagen,
+			String categoria, double precio, int idproductos) {
+		super(idDirecciones, pais, estado, municipio, cpostal, calle, numeroext, idUsuarios, usuario, correo,
+				contraseña, idProvedor, tel, ine, rfc, dirPago);
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		URL_imagen = uRL_imagen;
 		this.categoria = categoria;
 		this.precio = precio;
 		total ++;
-		this.id=total;
-	}// constructor
-	
-	public productos() {
-		total++;
-		this.id=total;
-	}//Producto
-	public int getId() {
-		return id;
-	}//getId
+		this.idproductos = total;
+		
+	}// constructorProductos
 	public String getNombre() {
 		return nombre;
-	}//getNombre
+	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}//setNombre
+	}
 	public String getDescripcion() {
 		return descripcion;
-	}//getDescripcion
+	}
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
-	}//setDescripcion
+	}
 	public String getURL_imagen() {
 		return URL_imagen;
-	}//getURL_imagen
+	}
 	public void setURL_imagen(String uRL_imagen) {
 		URL_imagen = uRL_imagen;
-	}//setURL_imagen
+	}
 	public String getCategoria() {
 		return categoria;
-	}//getCategoria
+	}
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
-	}//setCategoria
+	}
 	public double getPrecio() {
 		return precio;
-	}//getPrecio
+	}
 	public void setPrecio(double precio) {
 		this.precio = precio;
-	}//setPrecio
-
+	}
+	public int getIdproductos() {
+		return idproductos;
+	}
+	
 	@Override
 	public String toString() {
-		return "productos [nombre=" + nombre + ", descripcion=" + descripcion + ", URL_imagen=" + URL_imagen
-				+ ", categoria=" + categoria + ", precio=" + precio + "]";
-	}//toString productos
+		return "productos [getNombre()=" + getNombre() + ", getDescripcion()=" + getDescripcion() + ", getURL_imagen()="
+				+ getURL_imagen() + ", getCategoria()=" + getCategoria() + ", getPrecio()=" + getPrecio()
+				+ ", getIdproductos()=" + getIdproductos() + "]";
+	}//toStringProductos
 	
-}//class productos
+
+	
+}//classProductos
 
 

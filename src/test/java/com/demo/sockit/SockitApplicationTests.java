dd9e113d5452530005b7d2377fc3efbb2ee65081
claudio <claudiobalbuenasamano@gmail.com>
2022-07-06 16:21:41 -0500

    Se sube el pojo direcciones

---
+++ src/test/java/com/demo/sockit/SockitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sockit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
